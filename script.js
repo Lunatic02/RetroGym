d2e1bdd13cbfdc4048b1782e3f8839d436cf042c
Lunatic02 <lucas_hcosta@live.com>
2022-06-06 20:28:50 -0300

    Primeiro Commit RetroGym

---
+++ script.js
@@ -0,0 +1,42 @@
+function initScrollSuave(){
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+
+}
+
+linksInternos.forEach((link)=>{
+link.addEventListener('click', scrollToSection);
+})
+};
+initScrollSuave();  
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+if(sections.length){
+
+const windowMetade = window.innerHeight * 0.8;
+
+function animaScroll(){ 
+    sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        if(sectionTop<0){
+            section.classList.add('ativo')
+        }
+    })
+}
+animaScroll();
+
+
+window.addEventListener('scroll', animaScroll)
+}
+}
+
+initAnimacaoScroll();
